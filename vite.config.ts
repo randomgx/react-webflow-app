6bed08f3d8abfe0c6504ed8d47376ba95584036d
Lucas <lucasramalho360@gmail.com>
2024-02-23 17:03:30 -0300

    Added bridge, and __webflow.json copy to dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: path.resolve(__dirname, "./webflow.json"),
+          dest: path.resolve(__dirname, "./dist"),
+          rename: "__webflow.json",
+        },
+      ],
+    }),
+  ],
+});
